17ba0f2b219519c21f1fb205d5c1bea07b76aa29
FloMorice <florentin.morice2021@campus-eni.fr>
2021-06-28 15:17:03 +0200

    update inscription avec validation des données saisies avec regex

--- src/fr/eni/servlets/InscriptionServlet.java
+++ src/fr/eni/servlets/InscriptionServlet.java
@@ -13,6 +13,10 @@ import java.util.regex.Pattern;
 
 @WebServlet("/inscription")
 public class InscriptionServlet extends HttpServlet {
+    private static Pattern nomPattern = Pattern.compile("\\p{L}*(-\\p{L}*)*");
+    private static Pattern mailPattern = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    private static Pattern telPattern = Pattern.compile("^(\\d{2}[- .]?){5}$");
+    private static Pattern cpPattern = Pattern.compile("(\\d{2}[ ]?)+(\\d{3})");
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("WEB-INF/jsp/inscription.jsp").forward(request, response);
@@ -31,12 +35,36 @@ public class InscriptionServlet extends HttpServlet {
             String ville = request.getParameter("ville");
             String password = request.getParameter("password");
             String confirmpass = request.getParameter("confirmpass");
-            Pattern pattern = Pattern.compile("[a-zA-Z]");
-            Matcher matcher = pattern.matcher(nom);
-            if(!matcher.matches()){
-                request.setAttribute("message", "Un nom ne doit contenir que des lettres");
-            }
-            if (password.equals(confirmpass)) {
+            Matcher nomMatcher = nomPattern.matcher(nom);
+            Matcher prenomMatcher = nomPattern.matcher(prenom);
+            Matcher mailMatcher = mailPattern.matcher(email);
+            Matcher telMatcher = telPattern.matcher(tel);
+            Matcher cpMatcher = cpPattern.matcher(codepostal);
+
+            if (!(nomMatcher.matches())) {
+                request.setAttribute("message", "Nom invalide");
+                System.out.println(nomMatcher.matches());
+                this.doGet(request, response);
+            } else if (!(prenomMatcher.matches())) {
+                request.setAttribute("message", "Prenom invalide");
+                System.out.println(prenomMatcher.matches());
+                this.doGet(request, response);
+            } else if (!(mailMatcher.matches())) {
+                request.setAttribute("message", "Adresse mail invalide");
+                System.out.println(prenomMatcher.matches());
+                this.doGet(request, response);
+            } else if (!(telMatcher.matches())) {
+                request.setAttribute("message", "Numéro de teléphone invalide");
+                System.out.println(prenomMatcher.matches());
+                this.doGet(request, response);
+            } else if (!(cpMatcher.matches())) {
+                request.setAttribute("message", "Code postal invalide");
+                System.out.println(prenomMatcher.matches());
+                this.doGet(request, response);
+            }else if (!password.equals(confirmpass)) {
+                request.setAttribute("message", "Les mots de passe ne correspondent pas");
+                request.getRequestDispatcher("WEB-INF/jsp/inscription.jsp").forward(request, response);
+            } else {
                 UtilisateurManager utilisateurManager = new UtilisateurManager();
                 Utilisateur utilisateur = new Utilisateur(pseudo, nom, prenom, email, tel, rue, codepostal, ville, password);
                 utilisateurManager.inscrireUtilisateur(utilisateur);
@@ -44,9 +72,6 @@ public class InscriptionServlet extends HttpServlet {
                 session.setAttribute("utilisateur", utilisateur);
                 session.setAttribute("connecte", true);
                 response.sendRedirect(request.getContextPath()+"/accueil");
-            } else {
-                request.setAttribute("message", "Les mots de passe ne correspondent pas");
-                request.getRequestDispatcher("WEB-INF/jsp/inscription.jsp").forward(request, response);
             }
         } catch(BLLException e){
                 request.setAttribute("message", e.getMessage());
