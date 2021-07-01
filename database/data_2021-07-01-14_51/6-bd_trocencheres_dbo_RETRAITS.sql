cf9e5d368a9d4423a6d35cc02630fa083e888874
rverdon2021 <renaud.verdon2021@campus-eni.fr>
2021-07-01 15:53:29 +0200

    docs: maj jeu de données

--- database/data_2021-07-01-14_51/6-bd_trocencheres_dbo_RETRAITS.sql
+++ database/data_2021-07-01-14_51/6-bd_trocencheres_dbo_RETRAITS.sql
@@ -3,3 +3,4 @@ INSERT INTO bd_trocencheres.dbo.RETRAITS (no_article, rue, code_postal, ville) V
 INSERT INTO bd_trocencheres.dbo.RETRAITS (no_article, rue, code_postal, ville) VALUES (3, N'Château des Ducs', N'44000', N'Nantes');
 INSERT INTO bd_trocencheres.dbo.RETRAITS (no_article, rue, code_postal, ville) VALUES (4, N'Champs Elysées', N'75000', N'Paris');
 INSERT INTO bd_trocencheres.dbo.RETRAITS (no_article, rue, code_postal, ville) VALUES (6, N'dans la rue', N'75000', N'Paris');
+INSERT INTO bd_trocencheres.dbo.RETRAITS (no_article, rue, code_postal, ville) VALUES (9, N'Champs Elysées', N'75000', N'Paris');
