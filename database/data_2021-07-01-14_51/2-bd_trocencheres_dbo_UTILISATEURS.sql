66e701fccd6e07ac050aceeaf203f4779808eeea
rverdon2021 <renaud.verdon2021@campus-eni.fr>
2021-07-02 11:39:18 +0200

    docs: maj jeux de données

--- database/data_2021-07-01-14_51/2-bd_trocencheres_dbo_UTILISATEURS.sql
+++ database/data_2021-07-01-14_51/2-bd_trocencheres_dbo_UTILISATEURS.sql
@@ -11,12 +11,12 @@ VALUES (1, N'admin', N'De Bretagne', N'Anne', N'duchesse_anne@nantes.fr', N'', N
 INSERT INTO bd_trocencheres.dbo.UTILISATEURS (no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur)
 VALUES (2, N'enchèreMax', N'GATES', N'Bill', N'billgates@ms.com', N'0651489878', N'Champs Elysées', N'75000', N'Paris', N'123', 1000000, 0);
 INSERT INTO bd_trocencheres.dbo.UTILISATEURS (no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur)
-VALUES (3, N'enchèreLimite', N'Misérable', N'Gavroche', N'jai-pas-de-maison-pas-argent-mais-je-troque@mail.com', N'6578951232', N'dans la rue', N'75000', N'Paris', N'123', 15, 0);
+VALUES (3, N'enchèreLimite', N'Misérable', N'Gavroche', N'jai-pas-de-maison-pas-argent-mais-je-troque@mail.com', N'6578951232', N'dans la rue', N'75000', N'Paris', N'123', 33, 0);
 INSERT INTO bd_trocencheres.dbo.UTILISATEURS (no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur)
 VALUES (4, N'toDelete', N'Anonyme', N'Visiteur', N'adblock@free.fr', N'', N'chez moi', N'65123', N'Quelque-part', N'123', 0, 0);
 INSERT INTO bd_trocencheres.dbo.UTILISATEURS (no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur)
 VALUES (5, N'noAction', N'Petit-Curieux', N'Lucas', N'lucas-petit-curieux@monmail.com', N'0645987812', N'rue des lavandes', N'65123', N'Saint-Herblain', N'123', 100, 0);
 INSERT INTO bd_trocencheres.dbo.UTILISATEURS (no_utilisateur, pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur)
-VALUES (6, N'historique', N'Bonaparte', N'Napoléon', N'np@mafrance.fr', N'', N'Palais des Tuileries', N'75000', N'Paris', N'123', 500, 0);
+VALUES (6, N'historique', N'Bonaparte', N'Napoléon', N'npb@mafrance.fr', N'', N'Palais des Tuileries', N'75000', N'Paris', N'123', 486, 0);
 
 SET IDENTITY_INSERT bd_trocencheres.dbo.UTILISATEURS OFF;
--- database/data_2021-07-01-14_51/5-bd_trocencheres_dbo_ENCHERES.sql
+++ database/data_2021-07-01-14_51/5-bd_trocencheres_dbo_ENCHERES.sql
@@ -1,8 +1,12 @@
 INSERT INTO bd_trocencheres.dbo.ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere)
-VALUES (-1, 9, N'2018-08-06 14:34:34.000', 45321);
+VALUES (-1, 9, N'2018-06-08 14:34:34.000', 45321);
 INSERT INTO bd_trocencheres.dbo.ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere)
-VALUES (2, 2, N'1970-10-01 13:53:42.000', 431000);
+VALUES (2, 2, N'1970-01-10 13:53:42.000', 431000);
 INSERT INTO bd_trocencheres.dbo.ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere)
-VALUES (3, 3, N'2021-07-01 14:09:20.000', 13);
+VALUES (3, 3, N'2021-01-07 14:09:20.000', 13);
 INSERT INTO bd_trocencheres.dbo.ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere)
-VALUES (6, 3, N'2021-07-01 14:08:08.000', 12);
+VALUES (5, 10, N'2021-07-01 10:52:38.000', 513870);
+INSERT INTO bd_trocencheres.dbo.ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere)
+VALUES (6, 3, N'2021-07-02 00:00:00.000', 14);
+INSERT INTO bd_trocencheres.dbo.ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere)
+VALUES (6, 6, N'2018-09-02 10:18:38.000', 12800);
